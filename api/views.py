c1fafd2793774710a5eb0c1856bc78a27311cb56
Parth Krishna <thenerdsuperuser@gmail.com>
2021-02-14 21:56:55 +0530

    Added home view and commands

--- api/views.py
+++ api/views.py
@@ -3,10 +3,22 @@ from flask import Blueprint, jsonify
 main = Blueprint('main', __name__)
 
 
+@main.route('/')
+def home():
+    return """
+        <pre>
+        No UI. Run HTTP API calls only.
+        1. http http://localhost:3000/movies
+        2. http POST http://localhost:3000/add_movie
+        </pre>
+        """
+
+
 @main.route('/add_movie', methods=['POST'])
 def add_movie():
 
     return 'Done', 201
+
 
 @main.route('/movies')
 def movies():
